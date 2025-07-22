d1f65def06ba3418b6f2bc263cfaa640bce0656a
rodsilca <cabralrodrigo787@gmail.com>
2025-07-22 14:44:15 -0400

    User entity and resource

---
+++ src/main/java/com/rodsproject/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.rodsproject.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodsproject.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "fdsfd", "fdsfsdfsdfsdf", "fdsfdsfds");
+		return ResponseEntity.ok().body(user);
+	}
+}
