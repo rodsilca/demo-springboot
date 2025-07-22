dce94a061243a14b3c1cc39647b51ea2bee64b58
rodsilca <cabralrodrigo787@gmail.com>
2025-07-22 15:52:24 -0400

    Service Layer

---
+++ src/main/java/com/rodsproject/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.rodsproject.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodsproject.course.entities.User;
+import com.rodsproject.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		 return obj.get();
+	}
+}
