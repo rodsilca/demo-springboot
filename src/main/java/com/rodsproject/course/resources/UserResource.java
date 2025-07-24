587132e4e4cacdd5a87e1dd29e77c1bc82bb4553
rodsilca <cabralrodrigo787@gmail.com>
2025-07-24 14:28:15 -0400

    OrderStatus enum

--- src/main/java/com/rodsproject/course/resources/UserResource.java
+++ src/main/java/com/rodsproject/course/resources/UserResource.java
@@ -21,7 +21,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		List<User> list = service .findAll();
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
