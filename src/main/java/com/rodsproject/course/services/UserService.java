d70a99e97f05dd46cdde62d7e0eac79c5d5a7b3e
rodsilca <cabralrodrigo787@gmail.com>
2025-07-25 19:00:55 -0400

    User insert and delete

--- src/main/java/com/rodsproject/course/services/UserService.java
+++ src/main/java/com/rodsproject/course/services/UserService.java
@@ -22,4 +22,12 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		 return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
 }
