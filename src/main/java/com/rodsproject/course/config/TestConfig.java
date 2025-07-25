e961502f699df475abc454b8e0ad78d07edfe7e3
rodsilca <cabralrodrigo787@gmail.com>
2025-07-25 18:21:46 -0400

    Payment, one to one association

--- src/main/java/com/rodsproject/course/config/TestConfig.java
+++ src/main/java/com/rodsproject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rodsproject.course.entities.Category;
 import com.rodsproject.course.entities.Order;
 import com.rodsproject.course.entities.OrderItem;
+import com.rodsproject.course.entities.Payment;
 import com.rodsproject.course.entities.Product;
 import com.rodsproject.course.entities.User;
 import com.rodsproject.course.entities.enums.OrderStatus;
@@ -82,5 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2025-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+	
 	}
 }
