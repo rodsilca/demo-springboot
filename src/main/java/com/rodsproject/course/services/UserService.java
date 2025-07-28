24c6c80b5bee14d4e48efb5e14e09974920b62a4
rodsilca <cabralrodrigo787@gmail.com>
2025-07-28 15:18:19 -0400

    Exception Handling - delete

--- src/main/java/com/rodsproject/course/services/UserService.java
+++ src/main/java/com/rodsproject/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rodsproject.course.entities.User;
 import com.rodsproject.course.repositories.UserRepository;
+import com.rodsproject.course.services.exceptions.DatabaseException;
 import com.rodsproject.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +31,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+		try {
+			userRepository.deleteById(id);
+		}catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id); 
+		}catch (DataIntegrityViolationException e ) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
