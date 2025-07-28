14e89bc800298b8cf23da760cfb75684b13b2b2c
rodsilca <cabralrodrigo787@gmail.com>
2025-07-28 14:46:43 -0400

    Exception Handling - findById

--- src/main/java/com/rodsproject/course/entities/Payment.java
+++ src/main/java/com/rodsproject/course/entities/Payment.java
@@ -22,6 +22,7 @@ public class Payment implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private Instant moment;
 	
 	@JsonIgnore
--- src/main/java/com/rodsproject/course/services/UserService.java
+++ src/main/java/com/rodsproject/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.rodsproject.course.entities.User;
 import com.rodsproject.course.repositories.UserRepository;
+import com.rodsproject.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		 return obj.get();
+		 return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
